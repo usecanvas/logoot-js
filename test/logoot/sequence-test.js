44776e0aafe90e4f6402a419b7b776e0f877be8b
Jonathan Clem <jotclem@gmail.com>
2016-08-22 10:58:56 -0400

    Add Sequence.genAtomIdent

--- test/logoot/sequence-test.js
+++ test/logoot/sequence-test.js
@@ -37,4 +37,20 @@ describe('Sequende', function() {
       )
     });
   });
+
+  describe('.genAtomIdent', function() {
+    it('returns a new atom ident between the two given', function() {
+      const atomIdent = Sequence.genAtomIdent(1, 1, Sequence.min, Sequence.max);
+      assertGreaterAtomIdent(atomIdent, Sequence.min);
+      assertLesserAtomIdent(atomIdent, Sequence.max);
+    });
+  });
 });
+
+function assertGreaterAtomIdent(identA, identB) {
+  return assert.equal(Sequence.compareAtomIdents(identA, identB), 1);
+}
+
+function assertLesserAtomIdent(identA, identB) {
+  return assert.equal(Sequence.compareAtomIdents(identA, identB), -1);
+}
